ddc53b3047fe139896b08f1d973e78d7f6088dbe
alex <m.krokalex@gmail.com>
2022-07-24 11:26:42 +0300

    minor change env

--- runme.sh
+++ runme.sh
@@ -1,14 +1,12 @@
+git clone https://github.com/mr-quacker/dockerRepo.git
+cd dockerRepo
 mkdir src
 cd src
+mv env .env
 git clone https://github.com/mr-quacker/xmRepo.git
 cd xmRepo
 mv * ..
 cd ..
 rm -rf xmRepo
-mv Docker* ..
-mv docker* ..
 cd ..
-mkdir mysql
-mkdir nginx
-touch nginx/default.conf
 docker-compose up -d --build
