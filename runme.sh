aed8f4cca7d540f5df2a1b23108504a21955308a
alex <m.krokalex@gmail.com>
2022-07-24 13:02:26 +0300

    typo

--- runme.sh
+++ runme.sh
@@ -5,5 +5,5 @@ cd src
 mv env .env
 cd ..
 docker-compose up -d --build
-docker exec php compose install
-docker exec php php artisnan key:generate
+docker exec php composer install
+docker exec php php artisan key:generate
